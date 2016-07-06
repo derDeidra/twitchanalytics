3c2928312ede453d9f30b8afb5caa945367e5d0e
Ezra Dowd <ezra.dowd@gmail.com>
2016-07-05 20:06:35 -0400

    Made links relative

--- app/controllers/auth.server.controller.js
+++ app/controllers/auth.server.controller.js
@@ -14,13 +14,13 @@ exports.handleRedirect = function(req, res) {
         twitch.getAccessToken(req.query.code, function(err, body){
             if (err){
                 console.log(err);
-                res.redirect('/');
+                res.redirect('');
             } else {
                 console.log('[AUTH] Got access token ' + body.access_token);
                 twitch.getAuthenticatedUser(body.access_token, function(err, user_data){
                     if(err){
                         console.log('[AUTH] Error: Unable to access authenticated user object for auth token ' + body.access_token);
-                        res.redirect('/');
+                        res.redirect('');
                     } else {
                         console.log('[AUTH] Got channel information for user: ' + user_data.display_name);
                         req.session.display_name = user_data.display_name;
@@ -28,21 +28,21 @@ exports.handleRedirect = function(req, res) {
                         User.findOne({username : user_data.display_name}, function(err, user){
                             if(err){
                                 console.log("[AUTH] Error getting user object for " + user_data.display_name);
-                                res.redirect('/');
+                                res.redirect('');
                             } else {
                                 if(user != null){
                                     User.update({_id : user._id}, {auth : body.access_token});
                                     req.session.userid = user._id
-                                    res.redirect('/live');
+                                    res.redirect('live');
                                 } else {
                                     User.create({username : user_data.display_name, auth : body.access_token, tasks : []}, function(err, newUser){
                                         if(err) {
                                             console.log("[AUTH] Error creating user");
-                                            res.redirect('/');
+                                            res.redirect('');
                                         }
                                         else {
                                             req.session.userid = newUser._id;
-                                            res.redirect('/live');
+                                            res.redirect('live');
                                         }
                                     });
                                 }
@@ -59,7 +59,7 @@ exports.handleRedirect = function(req, res) {
 
 exports.accessRedirect = function(req, res, next){
     if(req.session.display_name){
-        res.redirect('/live');
+        res.redirect('live');
     } else {
         next();
     }
@@ -69,7 +69,7 @@ exports.accessControl = function(req, res, next){
     if(req.session.display_name){
         next();
     } else {
-        res.redirect('/');
+        res.redirect('');
     }
 };
 
@@ -78,15 +78,15 @@ exports.adminAccessControl = function(req, res, next){
         if(authAdmins.indexOf(req.session.display_name) >= 0){
             next();
         } else {
-            res.redirect('/');
+            res.redirect('');
         }
     } else {
-        res.redirect('/');
+        res.redirect('');
     }
 };
 
 exports.logout = function(req, res){
     req.session.destroy(function(){
-        res.redirect('/');
+        res.redirect('');
     });
 };
